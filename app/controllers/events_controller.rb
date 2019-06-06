b333118090f9e60ed47c14239bb75c53e96c9e32
edddjunior <edsonbergamojunior@gmail.com>
2019-06-06 11:58:28 -0300

    permit user to see events without being signed in

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,7 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
 
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
 
   # GET /events
   # GET /events.json
