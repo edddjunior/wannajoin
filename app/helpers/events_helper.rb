81d96cbaefd6b34caee069e1a0d27e64da0435c4
edddjunior <edsonbergamojunior@gmail.com>
2019-06-12 00:17:18 -0300

    user options added

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -8,4 +8,14 @@ module EventsHelper
     @events.sort_by(created_at);reverse
   end
 
+  def host
+      @event.user.username
+  end
+
+  def user_options
+    if signed_in?
+      render partial: 'user_options' if current_user.username == host
+    end
+  end
+
 end
