a4f4caabfdb8cec91185b1428a6be85a6850d098
edddjunior <edsonbergamojunior@gmail.com>
2019-06-12 15:21:33 -0300

    user index and show added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  resources :users, :only => [ :index, :show ]
   root "events#index"
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   devise_scope :user do
     get "login", :to => "devise/sessions#new"
     get "logout", :to => "devise/sessions#destroy"
