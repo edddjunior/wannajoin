62439b26aa27a9a820c70c04020732aaf9b6b230
edddjunior <edsonbergamojunior@gmail.com>
2019-06-14 00:08:02 -0300

    BIG HUGE USERS BUG solved

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  resources :users, :only => [ :index, :show ]
   root "events#index"
-  devise_for :users, controllers: { registrations: 'users/registrations' }
+  devise_for :users
   devise_scope :user do
     get "login", :to => "devise/sessions#new"
     get "logout", :to => "devise/sessions#destroy"
     get "signup", :to => "devise/registrations#new"
   end
+  resources :users, :only => [ :index, :show ]
   resources :events
 end
