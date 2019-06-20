244c13b1ba14058af66cd38d90f9ee8346ecc6e5
edddjunior <edsonbergamojunior@gmail.com>
2019-06-20 13:07:17 -0300

    flash notifications added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,15 @@
 module ApplicationHelper
+  def bootstrap_class_for_flash(flash_type)
+    case flash_type
+    when 'notice'
+      'alert-success'
+    when 'error'
+      'alert-danger'
+    else
+      flash_type.to_s
+    end
+  end
+
   def show_header
     if current_page?(root_path)
       render :partial => 'layouts/search_content'
